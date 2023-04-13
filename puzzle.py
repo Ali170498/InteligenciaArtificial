752740e8efd49c3ff3de290be029575f80571057
Ali170498 <130515834+Ali170498@users.noreply.github.com>
2023-04-12 23:22:25 -0400

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,26 +1,26 @@
 import random
 import copy
 
-class Puzzle:
-    def __init__(self):
+class Puzzle: #definimos la clase Puzzle
+    def __init__(self): #el metodo init se inicializa el rompecabezas creando el tablero utilizando el metodo privado
         self.size = 3
-        self.board = self._create_board()
+        self.board = self._create_board() #create_board es para mezclar aleatoriamente los numeros
 
     def _create_board(self):
         numbers = list(range(1, self.size * self.size))
-        numbers.append(None)
-        random.shuffle(numbers)
+        numbers.append(None) #utilizamos None para representar la casilla en blanco
+        random.shuffle(numbers) # el metodo _create_board ultiliza la funcion random,shuffle() para mezclar aleatoriamente (numeros)
         board = []
         for i in range(0, self.size * self.size, self.size):
             row = numbers[i:i + self.size]
             board.append(row)
         return board
 
-    def display(self):
+    def display(self): #el metood display muestra el tablero en la consola
         for row in self.board:
             print(row)
 
-    def move(self, direction):
+    def move(self, direction): 
         row, col = self.find_blank()
         if direction == "up" and row > 0:
             self.board[row][col], self.board[row - 1][col] = self.board[row - 1][col], self.board[row][col]
@@ -31,13 +31,13 @@ class Puzzle:
         elif direction == "right" and col < self.size - 1:
             self.board[row][col], self.board[row][col + 1] = self.board[row][col + 1], self.board[row][col]
 
-    def find_blank(self):
+    def find_blank(self): # busca la posición de la casilla en blanco en el tablero y devuelve su fila y columna
         for row in range(self.size):
             for col in range(self.size):
                 if self.board[row][col] is None:
                     return row, col
 
-    def is_solved(self):
+    def is_solved(self): #verifica si el rompecabezas está resuelto
         flattened = [item for row in self.board for item in row]
         return flattened == list(range(1, self.size * self.size)) + [None]
 
@@ -47,10 +47,10 @@ if __name__ == "__main__":
     puzzle.display()
 
     # Lógica para jugar el rompecabezas, mover piezas y verificar si está resuelto
-    while not puzzle.is_solved():
+    while not puzzle.is_solved(): #while repite hasta que este resuelto y salga el mensaje de aviso
         direction = input("Ingresa la dirección para mover (up/down/left/right): ")
         puzzle.move(direction)
         print("Rompecabezas después del movimiento:")
-        puzzle.display()
+        puzzle.display() #display es un metodo para actualizar el tablero
 
     print("¡Rompecabezas resuelto!")
